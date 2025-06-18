c1ee23fc81e936fe76f152945ff6e69e9da156ba
Nick <44708582+ecornellnick@users.noreply.github.com>
2025-06-18 05:11:53 -0400

    Update index.js

--- index.js
+++ index.js
@@ -23,12 +23,12 @@ Do not ask if there are any more questions.`
     // try {
     //   input = await codioIDE.coachBot.input("Please paste the error message you want me to explain!")
     // } catch (e) {
-    //   if (e.message == "Cancelled") {
-    //     codioIDE.coachBot.write("Please feel free to have any other error messages explained!")
-    //     codioIDE.coachBot.showMenu()
-    //     return
-    //   }
-    // }
+      if (e.message == "Cancelled") {
+        //codioIDE.coachBot.write("Please feel free to have any other error messages explained!")
+        codioIDE.coachBot.showMenu()
+        return
+      }
+    //}
     
     // console.log(input)
 
